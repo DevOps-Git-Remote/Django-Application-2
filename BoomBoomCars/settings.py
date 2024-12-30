6574488705b1606d62d93dcbaaed130532e1a9c4
ArunsaiDevOps <163491061+ArunsaiDevOps@users.noreply.github.com>
2024-12-30 08:01:46 -0600

    Update settings.py

--- BoomBoomCars/settings.py
+++ BoomBoomCars/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2@k-0optm4_=#-xbdin!pl@1^rd#58emo=viq%jky%40ec)#8j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
